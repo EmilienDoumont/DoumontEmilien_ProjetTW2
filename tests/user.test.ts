6988ed67c181406ad58d6510ab833dc854a635d6
Mimi <mimi@heh.be>
2025-01-04 14:13:50 +0100

    ajout css part02

--- tests/user.test.ts
+++ tests/user.test.ts
@@ -4,15 +4,15 @@ describe('User', () => {
   test('should create a User instance from JSON', () => {
     const json = {
       id: 1,
-      name: 'Alice Dupont',
+      name: 'Olivia Marshall',
       role: 'buyer',
-      email: 'alice@example.com',
+      email: 'oli.marshall@icloud.com',
       password: 'hashed_password_123'
     };
     const user = User.fromJSON(json);
 
     expect(user).toBeInstanceOf(User);
-    expect(user.name).toBe('Alice Dupont');
-    expect(user.email).toBe('alice@example.com');
+    expect(user.name).toBe('Olivia Marshall');
+    expect(user.email).toBe('oli.marshall@icloud.com');
   });
 });
